a2785ae2de77eb0dcd316a40757e44b4af4b3b03
Mark PC <friazmarkanthony@gmail.com>
2017-02-06 23:03:51 +0800

    For prod

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'plmos',
+	'hostname' => 'mysql.hostinger.ph',
+	'username' => 'u655183432_lgcy',
+	'password' => 'legacy',
+	'database' => 'u655183432_lgcy',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
