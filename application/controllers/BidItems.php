ce869020d554568a598c59c4741d4cdeafa64bf3
MarkFriaz <friazmarkanthony@gmail.com>
2017-02-27 14:51:50 +0800

    changed the Biditems instead of BidItems

--- application/controllers/BidItems.php
+++ application/controllers/BidItems.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class BidItems extends CI_Controller {
+class Biditems extends CI_Controller {
  private $param;
  	function __construct(){
  		parent::__construct();
